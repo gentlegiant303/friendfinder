63bcd40e104b512fd987e2d4e8cb147f9ee55aa2
Ben Petrila <benjamin.petrila@gmail.com>
2018-02-19 11:47:13 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.text());
+
+require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
+
+app.listen(PORT, function() {
+  console.log("Find a friend on port: " + PORT);
+});
