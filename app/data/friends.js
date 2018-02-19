993dc7c0238cf5078ff0a692922d53f42a4b30e3
Ben Petrila <benjamin.petrila@gmail.com>
2018-02-19 11:48:06 -0700

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+let friendsArray = [{
+  name: 'Buddy',
+  photo: 'https://cdn.thinglink.me/api/image/792623855247032322/1240/10/scaletowidth',
+  scores: [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '5',
+    '4',
+    '3',
+    '2',
+    '1'
+  ]
+}, {
+  name: 'Jar Jar',
+  photo: 'http://www.mindspring.com/~ernestm/jarjar/jarjar2.jpg',
+  scores: [
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1'
+  ]
+}];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,32 @@
+let path = require("path");
+let friends = require("../data/friends.js");
+
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+  app.post("/api/friends", function(req, res) {
+    let userInput = req.body;
+    let userResponses = userInput.scores;
+    let matchName = '';
+    let matchImage = '';
+    let totalDifference = 10000;
+    for (let i = 0; i < friends.length; i++) {
+      let difference = 0;
+      for (let j = 0; j < userResponses.length; j++) {
+        difference += Math.abs(friends[i].scores[j] - userResponses[i]);
+      }
+      if (difference < totalDifference) {
+        totalDifference = difference;
+        matchName = friends[i].name;
+        matchImage = friends[i].photo;
+      }
+    }
+    friends.push(userInput);
+    res.json({
+      status: 'OK',
+      matchName: matchName,
+      matchImage: matchImage
+    });
+  });
+};
